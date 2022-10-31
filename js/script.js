747b1a1157c63da16579319bf1f70d7d2e450976
Beksultan <beksultan.sagnaev@gmail.com>
2022-10-31 22:39:33 +0600

    feat: 640px adaptive

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    phone = document.querySelector('.mobile'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.burger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('burger_active');
+        menu.classList.toggle('menu_active');
+        phone.classList.toggle('mobile_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('burger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
